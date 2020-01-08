58bb2b348d78d8d04efa924cab86991de0bec0f1
raohong <724736528@qq.com>
2020-01-08 22:19:49 +0800

    构建补充

---
+++ scripts/build.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const fs = require('fs');
+
+const root = path.dirname(__dirname);
+
+const files = [
+  'refresh.wxs',
+  'index.weapp.tsx',
+  'index.template.wxml',
+  'lodash.throttle/index.js',
+  'lodash.throttle/index.d.ts'
+];
+const deleteFiles = ['ComponentResizeObserver.tsx', 'index.h5.tsx'];
+
+function exec() {
+  const fromBase = path.join(root, 'src/components/List');
+  const toBase = path.join(root, 'dist/weapp/components/List');
+
+  fs.mkdirSync(path.join(toBase, 'lodash.throttle'));
+  deleteFiles.forEach(f => fs.unlinkSync(path.join(toBase, f)));
+  files.forEach(f =>
+    fs.copyFileSync(path.join(fromBase, f), path.join(toBase, f))
+  );
+
+  const indexFile = path.join(root, 'dist/weapp/index.ts');
+
+  const content = fs.readFileSync(indexFile).toString();
+
+  fs.writeFileSync(indexFile, content.replace('h5', 'weapp'));
+}
+
+exec();

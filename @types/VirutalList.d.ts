d88c18d7b3ec54350e80f2d10cf608222a14eb24
meszyouh <724736528@qq.com>
2020-01-13 15:58:48 +0800

    声明更新

--- @types/VirutalList.d.ts
+++ @types/VirutalList.d.ts
@@ -1,107 +1,95 @@
-declare namespace TaroVirutalList {
-  enum ALIGN {
-    START = 'start',
-    END = 'end',
-    CENTER = 'center',
-    Auto = 'auto'
-  }
-
-  enum DIRECTION {
-    VERTICAL = 'vertical',
-    HORIZONTAL = 'horizontal'
-  }
-
-  type ItemSize = number | number[] | ((index: number) => number);
-
-  type ItemSizeGetter = (indx: number) => number;
-
-  type EstimatedSizeGetter = () => number;
-
-  type ItemStylePosition = 'sticky' | 'absolute';
-
-  interface ItemStyle {
-    position: ItemStylePosition;
-    width: string | number;
-    height: string | number;
-    left: number;
-    top: number;
-    zIndex: React.CSSProperties['zIndex'];
-  }
-
-  interface VirtualListProps {
-    width: number | string;
-    height: number | string;
-    dynamic?: boolean;
-    align?: ALIGN;
-    scrollDirection?: DIRECTION;
-    scrollToIndex?: number;
-    dataManager: VirutalListDataManager;
-    onOffsetChange: (scrollTop: number) => void;
-  }
-
-  interface SizeAndPositionOfItemData {
-    index: number;
-    style: ItemStyle;
-  }
-
-  export interface VirutalListItemData<T = any>
-    extends SizeAndPositionOfItemData {
-    item: T;
-  }
-
-  type MiniItemSizeValue = string | number;
-  type MiniItemSize =
-    | MiniItemSizeValue
-    | MiniItemSizeValue[]
-    | ((index: number) => MiniItemSizeValue);
-
-  type VirutalListDataManagerChangeHandler<T> = (
-    items: VirutalListItemData<T>[]
-  ) => void;
-
-  type VirutalListDataManagerUpdater<T> = (
-    data: T[]
-  ) => VirutalListItemData<T>[];
-
-  interface VirutalListDataManagerOptions<T> {
-    itemSize?: MiniItemSize;
-    estimatedSize?: number;
-    stickyIndices?: number[];
-    overscan?: number;
-    column?: number;
-    onChange: VirutalListDataManagerChangeHandler<T>;
-  }
-
-  interface VirutalListDataManagerState<T> {
-    data: T[];
-    column: number;
-    itemSize: ItemSize;
-    overscan: number;
-    itemCount: number;
-    estimatedSize: number;
-    stickyIndices: number[];
-    onChange: VirutalListDataManagerChangeHandler<T>;
-  }
-
-  interface VirutalListDataManagerPropto<T> {
-    updateConfig: (config: Partial<VirutalListDataManagerOptions<T>>) => void;
-    getItemCount: () => number;
-    clear: () => void;
-    push: (...value: T[]) => number;
-    set: (...value: T[]) => void;
-    splice: (start: number, deleteCount: number, ...items: T[]) => T[];
-    get: () => T[];
-    pop: () => T | undefined;
-    forceUpdate: () => void;
-  }
-
-  export interface VirutalListDataManager<T = any> {
-    new (
-      options: VirutalListDataManagerOptions<T>
-    ): VirutalListDataManagerPropto<T>;
-  }
+export enum ALIGN {
+  START = 'start',
+  END = 'end',
+  CENTER = 'center',
+  Auto = 'auto'
 }
 
-export const VirutalListDataManager: TaroVirutalList.VirutalListDataManager;
+export enum DIRECTION {
+  VERTICAL = 'vertical',
+  HORIZONTAL = 'horizontal'
+}
 
-export type VirutalListItemData = TaroVirutalList.VirutalListItemData;
+type ItemSize = number | number[] | ((index: number) => number);
+
+type ItemSizeGetter = (indx: number) => number;
+
+type EstimatedSizeGetter = () => number;
+
+type ItemStylePosition = 'sticky' | 'absolute';
+
+interface ItemStyle {
+  position: ItemStylePosition;
+  width: string | number;
+  height: string | number;
+  left: number;
+  top: number;
+  zIndex: React.CSSProperties['zIndex'];
+}
+
+interface VirtualListProps {
+  width: number | string;
+  height: number | string;
+  dynamic?: boolean;
+  align?: ALIGN;
+  scrollDirection?: DIRECTION;
+  scrollToIndex?: number;
+  dataManager: VirutalListDataManager;
+  onOffsetChange: (scrollTop: number) => void;
+}
+
+interface SizeAndPositionOfItemData {
+  index: number;
+  style: ItemStyle;
+}
+
+export interface VirutalListItemData<T = any>
+  extends SizeAndPositionOfItemData {
+  item: T;
+}
+
+type MiniItemSizeValue = string | number;
+type MiniItemSize =
+  | MiniItemSizeValue
+  | MiniItemSizeValue[]
+  | ((index: number) => MiniItemSizeValue);
+
+type VirutalListDataManagerChangeHandler<T> = (
+  items: VirutalListItemData<T>[]
+) => void;
+
+type VirutalListDataManagerUpdater<T> = (data: T[]) => VirutalListItemData<T>[];
+
+interface VirutalListDataManagerOptions<T> {
+  itemSize?: MiniItemSize;
+  estimatedSize?: number;
+  stickyIndices?: number[];
+  overscan?: number;
+  column?: number;
+  onChange: VirutalListDataManagerChangeHandler<T>;
+}
+
+interface VirutalListDataManagerState<T> {
+  data: T[];
+  column: number;
+  itemSize: ItemSize;
+  overscan: number;
+  itemCount: number;
+  estimatedSize: number;
+  stickyIndices: number[];
+  onChange: VirutalListDataManagerChangeHandler<T>;
+}
+
+export class VirutalListDataManager<T = any> {
+  constructor(options: VirutalListDataManagerOptions<T>);
+
+  updateConfig: (config: Partial<VirutalListDataManagerOptions<T>>) => void;
+  getItemCount: () => number;
+  clear: () => void;
+  push: (...value: T[]) => number;
+  set: (...value: T[]) => void;
+  splice: (start: number, deleteCount: number, ...items: T[]) => T[];
+  get: () => T[];
+  pop: () => T | undefined;
+  forceUpdate: () => void;
+}
--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,6 +1,6 @@
 import { TaroList } from './List.d';
-import { VirutalListDataManager, VirutalListItemData } from './VirutalList.d';
+export * from './VirutalList.d';
 
 export default TaroList;
 
-export { TaroList, VirutalListDataManager, VirutalListItemData };
+export { TaroList };
